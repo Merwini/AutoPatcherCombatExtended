955b730570be7d7811902dc655149b9fc9e4831d
Merwini <86023141+Merwini@users.noreply.github.com>
2023-09-21 15:49:21 -0400

    Probably finished DefDataHolderGene, except for exporting

--- AutoPatcherCombatExtended/Source/DataHolders/DefDataHolderGene.cs
+++ AutoPatcherCombatExtended/Source/DataHolders/DefDataHolderGene.cs
@@ -11,10 +11,18 @@ namespace nuff.AutoPatcherCombatExtended
 {
     class DefDataHolderGene : DefDataHolder
     {
+        //TODO can genes add verbs? Need to look into whether they would need patching.
         public DefDataHolderGene(GeneDef def)
         {
+            this.def = def;
+            defName = def.defName;
+            parentModPackageId = def.modContentPack.PackageId;
+            modData = DataHolderUtil.ReturnModDataOrDefault(def);
 
+            GetOriginalData();
         }
+
+        internal GeneDef def;
 
         float original_ArmorRatingSharp;
         float original_ArmorRatingBlunt;
@@ -26,17 +34,22 @@ namespace nuff.AutoPatcherCombatExtended
 
         public override void GetOriginalData()
         {
-            //TODO
+            original_ArmorRatingSharp = def.statOffsets.GetStatValueFromList(StatDefOf.ArmorRating_Sharp, 0);
+            original_ArmorRatingBlunt = def.statOffsets.GetStatValueFromList(StatDefOf.ArmorRating_Blunt, 0);
+            original_ArmorRatingHeat = def.statOffsets.GetStatValueFromList(StatDefOf.ArmorRating_Heat, 0);
         }
 
         public override void AutoCalculate()
         {
-            //TODO
+            modified_ArmorRatingSharp = original_ArmorRatingSharp * modData.geneArmorSharpMult;
+            modified_ArmorRatingBlunt = original_ArmorRatingBlunt * modData.geneArmorBluntMult;
         }
 
         public override void Patch()
         {
-            //TODO
+            DataHolderUtil.AddOrChangeStat(def.statOffsets, StatDefOf.ArmorRating_Sharp, modified_ArmorRatingSharp);
+            DataHolderUtil.AddOrChangeStat(def.statOffsets, StatDefOf.ArmorRating_Blunt, modified_ArmorRatingBlunt);
+            DataHolderUtil.AddOrChangeStat(def.statOffsets, StatDefOf.ArmorRating_Heat, modified_ArmorRatingHeat);
         }
         public override StringBuilder PrepExport()
         {
@@ -49,5 +62,20 @@ namespace nuff.AutoPatcherCombatExtended
             //TODO
         }
 
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            if (Scribe.mode == LoadSaveMode.LoadingVars)
+            {
+                def = DefDatabase<GeneDef>.GetNamed(defName, false);
+                if (def != null)
+                {
+                    GetOriginalData();
+                }
+            }
+            Scribe_Values.Look(ref modified_ArmorRatingSharp, "modified_ArmorRatingSharp", 0);
+            Scribe_Values.Look(ref modified_ArmorRatingBlunt, "modified_ArmorRatingBlunt", 0f);
+            Scribe_Values.Look(ref modified_ArmorRatingHeat, "modified_ArmorRatingHeat", 0f);
+        }
     }
 }
