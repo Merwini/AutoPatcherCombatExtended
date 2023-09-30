f31e7364bb7c6386c71a5aece00106dbdd6f39b8
Merwini <86023141+Merwini@users.noreply.github.com>
2023-09-30 17:45:41 -0400

    Moved PatchVehicle into its own assembly, tp prevent errors when VF is not active

--- AutoPatcherCombatExtended/Source/APCEHarmonyPatches.cs
+++ AutoPatcherCombatExtended/Source/APCEHarmonyPatches.cs
@@ -4,39 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
+using RimWorld;
 using HarmonyLib;
 using System.Reflection;
-using Vehicles;
 
 namespace nuff.AutoPatcherCombatExtended
 {
     public class APCEHarmonyPatches
     {
+        public static Type VehicleDef;
+        public static Type VehicleTurretDef;
+        public static Type APCEPatchVehicle;
+        public static MethodInfo PatchVehicle;
+        public static MethodInfo PatchVehicleTurret;
+
         public APCEHarmonyPatches()
         {
+
             Harmony harmony = new Harmony("nuff.rimworld.autopatchercombatextended");
+
             if (ModsConfig.IsActive("SmashPhil.VehicleFramework"))
             {
+                foreach (ModContentPack mod in LoadedModManager.RunningModsListForReading)
+                {
+                    Log.Warning(mod.PackageId);
+                }
+                ModContentPack apcevf = LoadedModManager.RunningModsListForReading.First(mod => mod.PackageId.Equals("nuff.ceautopatcher"));
+                Assembly apcevfAssembly = apcevf.assemblies.loadedAssemblies.First(ass => ass.ToString().Contains("APCEVF"));
+                APCEPatchVehicle = apcevfAssembly.GetType("nuff.AutoPatcherCombatExtended.VF.APCEPatchVehicle");
+
+                ModContentPack vf = LoadedModManager.RunningModsListForReading.First(mod => mod.PackageId.Equals("smashphil.vehicleframework"));
+                Assembly vfAssembly = vf.assemblies.loadedAssemblies.First(ass => ass.ToString().Contains("Vehicles"));
+                VehicleDef = vfAssembly.GetType("Vehicles.VehicleDef");
+                VehicleTurretDef = vfAssembly.GetType("Vehicles.VehicleTurretDef");
+                PatchVehicle = APCEPatchVehicle.GetMethod("PatchVehicle");
+                PatchVehicleTurret = APCEPatchVehicle.GetMethod("PatchVehicleTurret");
+
                 MethodInfo original = typeof(APCEController).GetMethod(nameof(APCEController.DetermineDefType));
                 MethodInfo vPrefix = typeof(APCEVehiclePatch).GetMethod(nameof(APCEVehiclePatch.Prefix));
                 harmony.Patch(original, prefix: new HarmonyMethod(vPrefix));
             }
-        }
+        } 
     }
+
 
     //[HarmonyPatch(typeof(APCEController), nameof(APCEController.DetermineDefType))]
     public class APCEVehiclePatch
     {
         public static bool Prefix(ref Def def, ref APCEPatchLogger log)
         {
-            if (def is VehicleDef vehicle)
+            if (def.GetType() == APCEHarmonyPatches.VehicleDef)
             {
-                APCEPatchVehicle.PatchVehicle(vehicle, log);
+                APCEHarmonyPatches.PatchVehicle.Invoke(null, new object[] { def, log });
                 return false;
             }
-            else if (def is VehicleTurretDef turret)
+            else if (def.GetType() == APCEHarmonyPatches.VehicleTurretDef)
             {
-                APCEPatchVehicle.PatchVehicleTurret(turret, log);
+                APCEHarmonyPatches.PatchVehicleTurret.Invoke(null, new object[] { def, log });
                 return false;
             }
             return true;
