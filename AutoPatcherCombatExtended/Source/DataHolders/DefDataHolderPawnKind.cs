480ce85a961573f9fe3273801695dbea16df3498
Merwini <86023141+Merwini@users.noreply.github.com>
2023-12-07 15:35:17 -0500

    Made new DefDataHolderPawnKind

---
+++ AutoPatcherCombatExtended/Source/DataHolders/DefDataHolderPawnKind.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nuff.AutoPatcherCombatExtended
+{
+    class DefDataHolderPawnKind : DefDataHolder
+    {
+        public override void AutoCalculate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ExportXML()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GetOriginalData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Patch()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override StringBuilder PrepExport()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
