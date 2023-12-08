8058cd6aae49dbb10d3680bd80ceeeed27ea0591
Merwini <86023141+Merwini@users.noreply.github.com>
2023-12-08 15:58:38 -0500

    DefDataHolderBuild_TurretGun

--- AutoPatcherCombatExtended/Source/DataHolders/DefDataHolderBuilding_TurretGun.cs
+++ AutoPatcherCombatExtended/Source/DataHolders/DefDataHolderBuilding_TurretGun.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
+using RimWorld;
+using CombatExtended;
 
 namespace nuff.AutoPatcherCombatExtended
 {
@@ -13,7 +15,61 @@ namespace nuff.AutoPatcherCombatExtended
         {
         }
 
+        ThingDef turretBase;
+
+        float original_FillPercent;
+        float original_TurretBurstCooldownTime;
+        //ConceptDef original_SpawnedConcentLearnOpportunity;
+
+        float modified_FillPercent = 0.85f;
+        float modified_TurretBurstCooldownTime;
+        float modified_AimingAccuracy;
+        //ConceptDef modified_SpawnedConceptLearnOpportunity;
+        //string modified_SpawnedConceptLearnOpportunityString;
+
+        public override void GetOriginalData()
+        {
+            turretBase = def as ThingDef;
+
+            original_FillPercent = turretBase.fillPercent;
+            original_TurretBurstCooldownTime = turretBase.building.turretBurstCooldownTime;
+
+            //all this does is give you a Learning Helper lesson when you build one, not worth patching
+            //original_SpawnedConcentLearnOpportunity = turretBase.building.spawnedConceptLearnOpportunity;
+        }
+
         public override void AutoCalculate()
+        {
+            //if less than 0.85, will just set to the default 0.85, which is 1.49m in CE, enough to shoot over barricades but still be shot over by pawns.
+            if (original_FillPercent > 0.85)
+            {
+                modified_FillPercent = original_FillPercent;
+            }
+
+            if (!(turretBase.weaponTags == null) && (turretBase.weaponTags.Any(str => str.IndexOf("Artillery", StringComparison.OrdinalIgnoreCase) >= 0)))
+            {
+                modified_TurretBurstCooldownTime = 2;
+                //modified_SpawnedConceptLearnOpportunity = CE_ConceptDefOf.CE_MortarDirectFire;
+            }
+            else
+            {
+                modified_TurretBurstCooldownTime = original_TurretBurstCooldownTime * 0.5f;
+                //modified_SpawnedConceptLearnOpportunity = original_SpawnedConcentLearnOpportunity;
+            }
+
+            modified_AimingAccuracy = 1f;
+
+        }
+
+        //TODO
+        public override void Patch()
+        {
+            turretBase.fillPercent = modified_FillPercent;
+            turretBase.building.turretBurstCooldownTime = modified_TurretBurstCooldownTime;
+            DataHolderUtils.AddOrChangeStat(turretBase.statBases, CE_StatDefOf.AimingAccuracy, modified_AimingAccuracy);
+        }
+
+        public override StringBuilder PrepExport()
         {
             throw new NotImplementedException();
         }
@@ -23,20 +79,16 @@ namespace nuff.AutoPatcherCombatExtended
             throw new NotImplementedException();
         }
 
-        public override void GetOriginalData()
+        public override void ExposeData()
         {
-            throw new NotImplementedException();
-        }
-
-        //TODO
-        public override void Patch()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override StringBuilder PrepExport()
-        {
-            throw new NotImplementedException();
+            base.ExposeData();
+            if (Scribe.mode == LoadSaveMode.LoadingVars
+                || (Scribe.mode == LoadSaveMode.Saving && isCustomized == true))
+            {
+                Scribe_Values.Look(ref modified_FillPercent, "modified_FillPercent", original_FillPercent);
+                Scribe_Values.Look(ref modified_TurretBurstCooldownTime, "modified_TurretBurstCooldownTime", original_TurretBurstCooldownTime);
+                Scribe_Values.Look(ref modified_AimingAccuracy, "modified_AimingAccuracy", 1);
+            }
         }
     }
 }
