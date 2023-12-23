e336c40dc92cce18b7e21426df8120c490ba1d34
Merwini <86023141+Merwini@users.noreply.github.com>
2023-12-23 10:31:00 -0500

    DefDataHolderVehicleDef.GetOriginalData()

--- APCEVF/DefDataHolderVehicleDef.cs
+++ APCEVF/DefDataHolderVehicleDef.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
+using RimWorld;
+using Vehicles;
 
 namespace nuff.AutoPatcherCombatExtended.VF
 {
@@ -13,21 +15,66 @@ namespace nuff.AutoPatcherCombatExtended.VF
         {
         }
 
-        public override void AutoCalculate()
-        {
-            throw new NotImplementedException();
-        }
+        VehicleDef vehicleDef;
 
-        public override void ExportXML()
-        {
-            throw new NotImplementedException();
-        }
+        //original values
+        float original_ArmorRatingSharp;
+        float original_ArmorRatingBlunt;
+        float original_ArmorRatingHeat;
+
+        List<float> original_ComponentArmorSharps = new List<float>();
+        List<float> original_ComponentArmorBlunts = new List<float>();
+        List<int> original_ComponentHealths = new List<int>();
+
+        float original_CargoCapacity;
+        int cargoIndex;
+
+        //modified values
+        float modified_ArmorRatingSharp;
+        float modified_ArmorRatingBlunt;
+        float modified_ArmorRatingHeat;
+
+        List<float> modified_ComponentArmorSharps = new List<float>();
+        List<float> modified_ComponentArmorBlunts = new List<float>();
+        List<int> modified_ComponentHealths = new List<int>();
+
+        float modified_CargoCapacity;
 
         public override void GetOriginalData()
         {
-            throw new NotImplementedException();
+            vehicleDef = def as VehicleDef;
+
+            original_ArmorRatingSharp = vehicleDef.statBases.GetStatValueFromList(StatDefOf.ArmorRating_Sharp, 0);
+            original_ArmorRatingBlunt = vehicleDef.statBases.GetStatValueFromList(StatDefOf.ArmorRating_Blunt, 0);
+            original_ArmorRatingHeat = vehicleDef.statBases.GetStatValueFromList(StatDefOf.ArmorRating_Heat, 0);
+
+            original_ComponentArmorSharps.Clear();
+            original_ComponentArmorBlunts.Clear();
+            original_ComponentHealths.Clear();
+            for (int i = 0; i < vehicleDef.components.Count; i++)
+            {
+                original_ComponentArmorSharps.Add(vehicleDef.components[i].armor.GetStatValueFromList(StatDefOf.ArmorRating_Sharp, 0));
+                original_ComponentArmorBlunts.Add(vehicleDef.components[i].armor.GetStatValueFromList(StatDefOf.ArmorRating_Blunt, 0));
+                original_ComponentHealths.Add(vehicleDef.components[i].health);
+            }
+
+            for (int i = 0; i < vehicleDef.vehicleStats.Count; i++)
+            {
+                if (vehicleDef.vehicleStats[i].statDef == VehicleStatDefOf.CargoCapacity)
+                {
+                    cargoIndex = i;
+                    original_CargoCapacity = vehicleDef.vehicleStats[i].value;
+                    break;
+                }
+            }
+
         }
 
+        public override void AutoCalculate()
+        {
+            throw new NotImplementedException();
+        }  
+        
         //TODO
         public override void Patch()
         {
@@ -35,6 +82,11 @@ namespace nuff.AutoPatcherCombatExtended.VF
         }
 
         public override StringBuilder PrepExport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ExportXML()
         {
             throw new NotImplementedException();
         }
