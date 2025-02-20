b1867d7f088f9474476e89722cdf5375e827f000
Merwini <86023141+Merwini@users.noreply.github.com>
2025-02-20 17:13:53 -0500

    Harmony Patch to fix CompAmmoUser for weapons that have been customized. Also some comps that tried to do the same but failed

---
+++ AutoPatcherCombatExtended/Source/Comps/CompGunAmmoFixer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using CombatExtended;
+
+namespace nuff.AutoPatcherCombatExtended
+{
+    class CompGunAmmoFixer : ThingComp
+    {
+        //weapons don't tick... preserving this in case I want to repurpose the code
+        //this comp will try to fix ammo for patched guns that are on the ground / in storage and thus tickable. separate comp for pawns to fix their equipped guns.
+        CompAmmoUser compAmmo;
+        CompProperties_AmmoUser compPropAmmo;
+
+        public override void CompTick()
+        {
+            Log.Warning($"ticking comp on {parent.Label}");
+            compAmmo = parent.TryGetComp<CompAmmoUser>();
+            compPropAmmo = parent.def.comps.FirstOrDefault(comp => comp is CompProperties_AmmoUser) as CompProperties_AmmoUser;
+            if (compAmmo != null && compPropAmmo != null)
+            {
+                if (compAmmo.UseAmmo && (compAmmo.CurrentAmmo == null || !compPropAmmo.ammoSet.ammoTypes.Any(link => link.ammo == compAmmo.CurrentAmmo)))
+                {
+                    compAmmo.CurrentAmmo = compPropAmmo.ammoSet.ammoTypes.First().ammo;
+                }
+
+                if (compAmmo.CurAmmoCount > compPropAmmo.magazineSize)
+                {
+                    compAmmo.curMagCountInt = compPropAmmo.magazineSize;
+                }
+            }
+
+            //self-destruct when no longer needed, to not waste calculations ticking
+            parent.comps.RemoveAll(comp => comp is CompGunAmmoFixer);
+        }
+    }
+}
---
+++ AutoPatcherCombatExtended/Source/Comps/CompProperties_GunAmmoFixer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace nuff.AutoPatcherCombatExtended
+{
+    class CompProperties_GunAmmoFixer : CompProperties
+    {
+        public CompProperties_GunAmmoFixer()
+        {
+            compClass = typeof(CompGunAmmoFixer);
+        }
+    }
+}
