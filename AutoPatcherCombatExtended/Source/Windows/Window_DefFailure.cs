ffbf7ad581419af3bf8b0f1f369bae5f9bede3f3
Merwini <86023141+Merwini@users.noreply.github.com>
2025-04-17 10:16:17 -0400

    Window_DefFailure no longer throws endless exceptions

--- AutoPatcherCombatExtended/Source/Windows/Window_DefFailure.cs
+++ AutoPatcherCombatExtended/Source/Windows/Window_DefFailure.cs
@@ -16,12 +16,17 @@ namespace nuff.AutoPatcherCombatExtended
         public Window_DefFailure(Def def)
         {
             this.def = def;
+            doCloseButton = true;
         }
 
         public override void DoWindowContents(Rect inRect)
         {
-            //Show a failure message
-            throw new NotImplementedException();
+            Listing_Standard list = new Listing_Standard();
+            list.Begin(inRect);
+            Text.Font = GameFont.Medium;
+            Widgets.Label(new Rect(0f, 0f, inRect.width - 150f - 17f, 35f), $"Not sure how toc ustomize {def.defName}");
+            Text.Font = GameFont.Small;
+            list.End();
         }
     }
 }
